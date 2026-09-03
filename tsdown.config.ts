f3719bcd1ba6de12b737a1e57363a1de22d25dc8
Travis Zhang <wr.zhang25@gmail.com>
2026-09-03 20:14:20 +0800

    feat: update

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   platform: 'neutral',
   dts: true,
-  exports: true,
-  // ...config options
+  exports: {
+    customExports(exports) {
+      exports['./style.css'] = './dist/style.css'
+      exports['./src/style.css'] = './dist/style.css'
+      return exports
+    },
+  },
+  copy: [{ from: 'src/style.css', to: 'dist', flatten: true }],
 })
